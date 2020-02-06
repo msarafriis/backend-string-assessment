552445064e17bc8e70c8332e740a76375a19e678
Marie S. Friis <msfriis@outlook.com>
2020-02-06 11:13:19 -0500

    finished string2

--- string2.py
+++ string2.py
@@ -53,12 +53,24 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-    """Your code goes here.  Edit this docstring."""
-    return
-
+    if len(a) % 2 == 1:
+        a_front = a[:int((len(a) + 1)/2)]
+        a_back = a[int((len(a) + 1)/2):]
+    else:
+        a_front = a[:int(len(a)/2)]
+        a_back = a[int(len(a)/2):]
+    if len(b) % 2 == 1:
+        b_front = b[:int((len(b) + 1)/2)]
+        b_back = b[int((len(b) + 1)/2):]
+    else:
+        b_front = b[:int(len(b)/2)]
+        b_back = b[int(len(b)/2):]
+    return a_front + b_front + a_back + b_back
 
 # Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
+
+
 def test(got, expected):
     """Your code goes here.  Edit this docstring."""
     if got == expected:
